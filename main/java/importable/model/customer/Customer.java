4cb68c00604ac4a7cdb70bfe0c57f2c89d5cd114
AfonsoFernando051 <afonsofernando051@gmail.com>
2025-10-16 21:33:11 -0300

    Adding Customer model class and OldCustomerImporter class

---
+++ main/java/importable/model/customer/Customer.java
@@ -0,0 +1,104 @@
+package importable.model.customer;
+
+
+import java.time.LocalDate;
+
+/**
+ * Represents a customer entity with personal and contact information.
+ * Can be mapped from spreadsheet data (e.g., customers.xlsx).
+ */
+public class Customer {
+
+    /** Unique identifier of the customer. */
+    private Long id;
+
+    /** Full name of the customer. */
+    private String name;
+
+    /** CPF (Brazilian personal identification number). */
+    private String cpf;
+
+    /** Email address of the customer. */
+    private String email;
+
+    /** Customer's date of birth. */
+    private LocalDate birthDate;
+
+    /** City where the customer lives. */
+    private String city;
+
+    /** State (UF) corresponding to the customer's city. */
+    private String state;
+
+    /** Default constructor. */
+    public Customer() {}
+
+    /**
+     * Full constructor.
+     *
+     * @param id         the customer ID
+     * @param name       the customer's full name
+     * @param cpf        the CPF number
+     * @param email      the customer's email address
+     * @param birthDate  the customer's date of birth
+     * @param city       the customer's city
+     * @param state      the customer's state (UF)
+     */
+    public Customer(Long id, String name, String cpf, String email, LocalDate birthDate, String city, String state) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.email = email;
+        this.birthDate = birthDate;
+        this.city = city;
+        this.state = state;
+    }
+
+    /** @return the customer ID */
+    public Long getId() { return id; }
+
+    /** @param id the customer ID to set */
+    public void setId(Long id) { this.id = id; }
+
+    /** @return the customer's name */
+    public String getName() { return name; }
+
+    /** @param name the customer's name to set */
+    public void setName(String name) { this.name = name; }
+
+    /** @return the customer's CPF */
+    public String getCpf() { return cpf; }
+
+    /** @param cpf the customer's CPF to set */
+    public void setCpf(String cpf) { this.cpf = cpf; }
+
+    /** @return the customer's email */
+    public String getEmail() { return email; }
+
+    /** @param email the customer's email to set */
+    public void setEmail(String email) { this.email = email; }
+
+    /** @return the customer's date of birth */
+    public LocalDate getBirthDate() { return birthDate; }
+
+    /** @param birthDate the customer's date of birth to set */
+    public void setBirthDate(LocalDate birthDate) { this.birthDate = birthDate; }
+
+    /** @return the customer's city */
+    public String getCity() { return city; }
+
+    /** @param city the customer's city to set */
+    public void setCity(String city) { this.city = city; }
+
+    /** @return the customer's state (UF) */
+    public String getState() { return state; }
+
+    /** @param state the customer's state (UF) to set */
+    public void setState(String state) { this.state = state; }
+
+    /** @return a short string representation of the customer */
+    @Override
+    public String toString() {
+        return String.format("%d - %s (%s, %s)", id, name, city, state);
+    }
+}
---
+++ main/java/importable/old/OldCustomerImporter.java
@@ -0,0 +1,65 @@
+package importable.old;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import importable.model.customer.Customer;
+
+public class OldCustomerImporter {
+
+  /**
+   * Importação de dados da planilha de clientes
+   */
+	public static void importarDadosPlanilha(String abaPlanilha,
+                                     boolean possuiCabecalho,
+                                     String colunaNome,
+                                     String colunaEmail,
+                                     String colunaCidade,
+                                     byte[] buf) {
+    try {
+      ByteArrayInputStream inputStream = new ByteArrayInputStream(buf);
+      XSSFWorkbook workXssf = new XSSFWorkbook(inputStream);
+      int abaIndex = Character.getNumericValue(abaPlanilha.charAt(0));
+      XSSFSheet planilha = workXssf.getSheetAt(abaIndex);
+      Iterator<Row> rowIterator = planilha.iterator();
+      List<Customer> clientes = new ArrayList<Customer>();
+      final int POS_NOME = ((byte) colunaNome.toUpperCase().toCharArray()[0]) - 65;
+      final int POS_EMAIL = ((byte) colunaEmail.toUpperCase().toCharArray()[0]) - 65;
+      final int POS_CIDADE = ((byte) colunaCidade.toUpperCase().toCharArray()[0]) - 65;
+
+      while (rowIterator.hasNext()) {
+        Row row = rowIterator.next();
+        if (possuiCabecalho && row.getRowNum() < 1) {
+          continue;
+        }
+        Iterator<Cell> cellIterator = row.cellIterator();
+        Customer customer = new Customer();
+        while (cellIterator.hasNext()) {
+          Cell cell = cellIterator.next();
+          int columnIndex = cell.getColumnIndex();
+          if (columnIndex == POS_NOME) {
+            customer.setName(cell.getStringCellValue());
+          } else if (columnIndex == POS_EMAIL) {
+            customer.setEmail(cell.getStringCellValue());
+          } else if (columnIndex == POS_CIDADE) {
+            customer.setCity(cell.getStringCellValue());
+          }
+        }
+        clientes.add(customer);
+      }
+
+      workXssf.close();
+    } catch (EncryptedDocumentException | IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
